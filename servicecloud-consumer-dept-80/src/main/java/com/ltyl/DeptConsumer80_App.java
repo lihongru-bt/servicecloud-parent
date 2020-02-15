7cb441d177c89141588af0ae2406834f9324194c
lihongru-bt <1018652754@qq.com>
2020-02-15 23:07:40 +0800

    第一次提交

--- servicecloud-consumer-dept-80/src/main/java/com/ltyl/DeptConsumer80_App.java
+++ servicecloud-consumer-dept-80/src/main/java/com/ltyl/DeptConsumer80_App.java
@@ -3,6 +3,9 @@ package com.ltyl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class DeptConsumer80_App {
     public static void main(String[] args) {
